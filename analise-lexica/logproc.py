61d8652149c3bddae55c0a6f82adbfdbf62c0be3
Victor <victorfreitas26@gmail.com>
2020-09-29 23:28:12 -0300

    [WIP] codigo funciona para dividir texto, porem não passa no teste

--- analise-lexica/logproc.py
+++ analise-lexica/logproc.py
@@ -9,24 +9,27 @@ tokens = [
     'PROC',
     'MESSAGE'
 ] 
-
+t_ignore  = '\t\n'
 def t_TIMESTAMP(t):
     # Regular expression for TIMESTAMP
+    r'([0-9:.])+\s([0-9\-\+])+(?=\t)'
     return t
 
 def t_PROC(t):
     # Regular expression for PROC
-    t.value = t.value[1:len(t.value) - 1]
+    r'[\w -\.]+(?=\t)'
+    t.value = t.value[:len(t.value)]
     return t
 
 def t_MESSAGE(t):
     # Regular expression for MESSAGE
-    t.value = t.value[:len(t.value) - 1]
+    r'[^	]+(?=\n|$)'
+    t.value = t.value[:len(t.value)]
     return t
 
 # Error handling rule
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print("Illegal character '%s'" % t.value[0:100])
     t.lexer.skip(1)
 
 
@@ -35,14 +38,28 @@ class LogProcLexer:
     lexer = None
     def __init__(self):
         fh = open("log", 'r')
+        # fh = open("teste.txt", 'r')
         self.data = fh.read()
         fh.close()
         self.lexer = lex.lex()
         self.lexer.input(self.data)
 
     def collect_messages(self):
-        # Do your magic here
+        tokens = []
+        current_proc=''
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break      # No more input
+            if tok.type == 'PROC':
+                current_proc=tok.value
+            if tok.type == 'MESSAGE' and current_proc =='kernel':
+                tokens.append(tok)
+        return tokens
         
 if __name__ == '__main__':
+#     f = open("demofile3.txt", "w")
+# f.write(str(LogProcLexer().collect_messages()))
+# f.close()
     print(LogProcLexer().collect_messages())
     
