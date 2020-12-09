ea200c7d03ed1ae8c1b9df3bba4c5d45dd09ca2b
Victor <victorfreitas26@gmail.com>
2020-12-09 10:50:10 -0300

    casos de teste

--- geracao-codigo-intermediario/imp2-return-test.py
+++ geracao-codigo-intermediario/imp2-return-test.py
@@ -18,9 +18,15 @@ class TestPiAut(unittest.TestCase):
         (trace, step, out, dt) = run(pi_ast, color=False)
         self.assertEqual(str(out), resp)
 
-    def test_exp(self):
-        resp ="texto"
-        self.__test_pi_aut('exp-test0.imp2', resp)
+#testa fatorial de 15
+    def test_fat(self):
+        resp = "[1307674368000]"
+        self.__test_pi_aut('examples/fat-func.imp2', resp)
+
+#testa fibo de 15
+    def test_fibo(self):
+        resp = "[[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]]"
+        self.__test_pi_aut('examples/fibo-func.imp2', resp)
         
 if __name__ == '__main__':
     unittest.main()
